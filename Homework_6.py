45b1a12370d9ac8cecfc52c889a73a9a9b3cd414
outlander85 <ivivchenko@gmail.com>
2019-10-23 20:56:30 +0300

    add print(*re)

--- Homework_6.py
+++ Homework_6.py
@@ -14,8 +14,9 @@ def readMass(path):
 def printMass(mass):
     for i in range(len(mass)):
         for j in range(len(mass[i])):
-            print('%4s'%mass[i][j], end='')
+            print('%4s' % mass[i][j], end='')
         print()
+
 
 def printSumMass(mass):
     t = []
@@ -28,15 +29,37 @@ def printSumMass(mass):
 
     return t
 
+
 mass, N = readMass('file_hw_6_1.txt')
 printMass(mass)
-print('  --'*N)
+print('  --' * N)
 # printSumMass(mass)
 re = printSumMass(mass)
-print(re)
+print(*re)
+
 
 num = 1
 for i in range(1, len(re)):
     if re[i] < re[num]:
         num = i
 print(num)
+
+
+# file = open(r'file_hw_6_2.txt')
+#
+# N = file.readlines()
+# # print(N)
+#
+# d = {}
+# for line in file:
+#     words = line.strip().split(' - ')
+#     ru = words[0]
+#     en = words[1].split(', ')
+#     for key in en:
+#         if key in d:
+#             d[key].append(ru)
+#         else:
+#             d[key] = [ru]
+# file.close()
+
+
